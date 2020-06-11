e6cc0a3ac83574baea666b9b5722c367994f2784
kepha <kephamotari1@gmail.com>
2020-06-11 15:19:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Description<br>
+This is a Shopping app build in express, stripe and ejs. Payments were setup by Stripe API.
---
+++ server.js
@@ -0,0 +1,65 @@
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').load()
+// }
+
+const stripeSecretKey = "secret_key";
+const stripePublicKey = "public_key";
+
+console.log(stripeSecretKey, stripePublicKey);
+
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const stripe = require("stripe")(stripeSecretKey);
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/store", function (req, res) {
+  fs.readFile("items.json", function (error, data) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      res.render("store.ejs", {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data),
+      });
+    }
+  });
+});
+
+app.post("/purchase", function (req, res) {
+  fs.readFile("items.json", function (error, data) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      const itemsJson = JSON.parse(data);
+      const itemsArray = itemsJson.music.concat(itemsJson.merch);
+      let total = 0;
+      req.body.items.forEach(function (item) {
+        const itemJson = itemsArray.find(function (i) {
+          return i.id == item.id;
+        });
+        total = total + itemJson.price * item.quantity;
+      });
+
+      stripe.charges
+        .create({
+          amount: total,
+          source: req.body.stripeTokenId,
+          currency: "usd",
+        })
+        .then(function () {
+          console.log("Charge Successful");
+          res.json({ message: "Successfully purchased items" });
+        })
+        .catch(function () {
+          console.log("Charge Fail");
+          res.status(500).end();
+        });
+    }
+  });
+});
+
+app.listen(3000, () => console.log("Server started"));
